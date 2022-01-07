deb4fd5be3de22005c754a40be6af59c33c6ef79
kil rial <tritono@bk.ru>
2022-01-07 13:50:03 +0300

    master

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    use Entities\Articles;
+    use Entities\Categories;
+
+    include 'setup.php';
+    require_once('libs/bootstrap.php');
+
+    $categories = $entityManager->getRepository(Categories::class)->findAll();
+    $smarty->assign('categories', $categories);
+
+    $articles = $entityManager->getRepository(Articles::class)->findBy([], ['id' => 'DESC'], 2);
+    $smarty->assign('articles', $articles);
+
+    $arts = $entityManager->getRepository(Articles::class)->findBy([], ['id' => 'DESC']);
+    $smarty->assign('arts', $arts);
+
+    $smarty->debugging = true;
+    $smarty->display('index.tpl');
+?>
