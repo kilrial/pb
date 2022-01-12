d4997c2357bf3caa9ca47f5a5268446ad8e830e3
kil rial <tritono@bk.ru>
2022-01-12 17:31:39 +0300

    pb

--- libs/bootstrap.php
+++ libs/bootstrap.php
@@ -2,6 +2,7 @@
 // bootstrap.php
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
+use Dotenv\Dotenv;
 
 require_once "vendor/autoload.php";
 
@@ -16,12 +17,15 @@ $config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/src"), $i
 // $config = Setup::createYAMLMetadataConfiguration(array(__DIR__."/config/yaml"), $isDevMode);
 
 // database configuration parameters
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
+
 $conn = array(
-    'dbname' => 'blog_db',
-    'user' =>   'test',
-    'password' =>'Test123',
+    'dbname' => $_ENV['dbname'],
+    'user' =>   $_ENV['user'], 
+    'password' =>$_ENV['password'],
     'driver' => 'pdo_mysql',
-    'host' => 'localhost'
+    'host' => $_ENV['host']
 );
 
 $entityManager = EntityManager::create($conn, $config);
