70ffefbb256f76300bf015b232a69acae9828a5a
kil rial <tritono@bk.ru>
2022-02-16 20:07:45 +0300

    pb

--- public/templates_c/fa1d4a572185fdcff03754d7db571cbc2ad43cb8_0.file.sidebar.tpl.php
+++ public/templates_c/fa1d4a572185fdcff03754d7db571cbc2ad43cb8_0.file.sidebar.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.0.3, created on 2022-01-12 19:49:48
+/* Smarty version 4.0.3, created on 2022-01-15 20:05:46
   from '/var/www/pb/public/templates/sidebar.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.0.3',
-  'unifunc' => 'content_61df06acbe8a35_57829528',
+  'unifunc' => 'content_61e2feeaf20b40_71570926',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'fa1d4a572185fdcff03754d7db571cbc2ad43cb8' => 
     array (
       0 => '/var/www/pb/public/templates/sidebar.tpl',
-      1 => 1641649922,
+      1 => 1642266345,
       2 => 'file',
     ),
   ),
@@ -20,12 +20,17 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_61df06acbe8a35_57829528 (Smarty_Internal_Template $_smarty_tpl) {
+function content_61e2feeaf20b40_71570926 (Smarty_Internal_Template $_smarty_tpl) {
 ?><div class="col-md-4">
       <div class="position-sticky" style="top: 2rem;">
         <div class="p-4 mb-3 bg-light rounded">
-          <h4 class="fst-italic">About</h4>
-          <p class="mb-0">Customize this section to tell your visitors a little bit about your publication, writers, content, or something else entirely. Totally up to you.</p>
+          <h4 class="fst-italic">Погода</h4>
+          <p class="mb-0">Температура воздуха: <?php echo sprintf("%d",$_smarty_tpl->tpl_vars['weather']->value['main']['temp']);?>
+ ℃</p>
+          <p class="mb-0">Давление: <?php echo sprintf("%d",$_smarty_tpl->tpl_vars['weather']->value['main']['pressure']);?>
+ мм рт. ст.</p>
+          <p class="mb-0">Скорость ветра: <?php echo sprintf("%d",$_smarty_tpl->tpl_vars['weather']->value['wind']['speed']);?>
+ м/с</p>
         </div>
 
         <div class="p-4">
